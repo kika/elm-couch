409ba426dfb4e64ee300bcba34526f11e9809326
Kirill Pertsev <kika@rackmaze.com>
2016-01-08 02:56:06 -0800

    make it compile with 0.16 and minor fixes

--- src/CouchDb.elm
+++ src/CouchDb.elm
@@ -1,71 +1,77 @@
-module CouchDB where
+module CouchDB (..) where
 
 import Graphics.Element exposing (show)
 import Http
 import Json.Decode as JD exposing ((:=))
 import Json.Encode as JE
 import Task exposing (Task, andThen, toResult, toMaybe)
+import TaskTutorial exposing (print)
 import Base64
-
 import Debug
 
-type alias Config = 
-  {
-    dbhost : String
-    , db     : String
-    , user   : String
-    , pass   : String
+
+type alias Config =
+  { dbhost : String
+  , db : String
+  , user : String
+  , pass : String
   }
 
+
 config : Config
-config = 
-  {
-    dbhost = "http://localhost:5984"
-    , db   = "qts"
+config =
+  { dbhost = "http://localhost:5984"
+    , db = "qts"
     , user = "root"
     , pass = "root123"
   }
 
-type alias State =
- {
-   session   : Maybe String
-   -- The seq number/id of the last seen _changes event
-   last_seen : String
- }
 
-type Cmd = 
-  Login
+type alias State =
+  { session : Maybe String
+    -- Seq number/id of the last _changes eeent
+    , last_seen : String
+  }
+
+
+type Cmd
+  = Login
   | Logout
   | Changes
 
 
 login : Task Http.Error Bool
-login = 
-  let req =
-    {
-      verb = "POST"
-      , headers = [("Content-Type", "application/json")]
-      , url = (config.dbhost ++ "/_session")
-      , body = Http.string
-                (JE.encode 0 
-                  (JE.object 
-                    [("name",     (JE.string config.user)), 
-                     ("password", (JE.string config.pass))
-                    ]))
-    }
-  in 
+login =
+  let
+    req =
+      { verb = "POST"
+        , headers = [ ( "Content-Type", "application/json" ) ]
+        , url = (config.dbhost ++ "/_session")
+        , body =
+          Http.string
+            (JE.encode
+              0
+              (JE.object
+                [ ( "name", (JE.string config.user) )
+                , ( "password", (JE.string config.pass) )
+                ]
+              )
+            )
+      }
+  in
     Http.fromJson ("ok" := JD.bool) (Http.send Http.defaultSettings req)
+
 
 changes : Task Http.Error (List String)
 changes =
-  let req =
-    {
-        verb = "GET",
-        headers = [],
-        url = config.dbhost ++ "/" ++ config.db ++ "/_changes?feed=longpoll",
-        body = Http.empty
-    }
-  in 
+  let
+    req =
+      { verb = "GET"
+        , headers = []
+        , url = config.dbhost ++ "/" ++ config.db ++ "/_changes?feed=longpoll"
+        , body = Http.empty
+      }
+  in
     Http.fromJson (JD.list JD.string) (Http.send Http.defaultSettings req)
 
 
@@ -73,18 +79,22 @@ doLogin : Task x (Result Http.Error Bool)
 doLogin =
   Task.toResult login
 
+
 doChanges : Task x (Result Http.Error (List String))
-doChanges = 
+doChanges =
   Task.toResult changes
+
 
 port runner : Task x ()
 port runner =
   doLogin
     `andThen` \status -> doChanges
-    `andThen` \result -> Debug.log result
+    `andThen` \result -> print result
 
 
 main =
   show "Open the Developer Console of your browser."
+
+
 
 -- vim: ts=2:sw=2:et
